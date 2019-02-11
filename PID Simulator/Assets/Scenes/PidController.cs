582ea13c102c2838b930958c56912904658f4409
zjjc123 <zjjc123@hotmail.com>
2019-02-10 23:06:39 -0800

    Added Basic Pid Control

---
+++ PID Simulator/Assets/Scenes/PidController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PidController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject pidObject;
+
+    MovementScript mov;
+
+
+    // ==== Values That You Have Access To ======================
+    float altitude;
+    float target;
+    // ==========================================================
+
+
+
+    // ======== Constants (EDIT) ==========
+    [SerializeField]
+    float Kp = 1f;
+    [SerializeField]
+    float Ki = 0f;
+    [SerializeField]
+    float Kd = 0f;
+
+    float totalError = 0f;
+    float lastError;
+   
+    // =============================
+
+
+
+
+    void Start()
+    {
+        mov = pidObject.GetComponent<MovementScript>();
+
+        // ======== Code (EDIT) =======================
+        lastError = altitude - target;
+
+
+
+        // ============================================
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update values automatically
+        altitude = mov.GetAltitude();
+        target = mov.GetTarget();
+
+
+        // Set Power using
+        // mov.setPower(10f);
+
+
+        // ======== Code (EDIT) =======================
+
+        float error = target - altitude;
+
+
+        float proportional = Kp * error;
+
+
+        totalError += error;
+        float integral = Ki * totalError;
+
+        float derivative = Kd*(error - lastError);
+        float power = derivative + integral + proportional;
+
+        mov.setPower(power);
+
+        lastError = error;
+
+        // =============================================
+    }
+}
---
+++ PID Simulator/Assets/Scenes/Scripts/MovementScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody rb;
+
+    float targetAltitude = 100;
+    float currentPower = 0;
+
+    [SerializeField]
+    GameObject slider;
+
+    [SerializeField]
+    GameObject targetText;
+
+    [SerializeField]
+    GameObject currentText;
+
+    [SerializeField]
+    GameObject PowerSlider;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        Debug.Log(rb);
+    }
+
+
+    private void Update()
+    {
+        targetAltitude = slider.GetComponent<Slider>().value;
+        targetText.GetComponent<Text>().text = string.Format("{0:N2}", targetAltitude);
+        currentText.GetComponent<Text>().text = string.Format("{0:N2}", transform.position.y);
+        PowerSlider.GetComponent<Slider>().value = currentPower;
+
+        rb.AddForce(new Vector3(0, currentPower, 0));
+        // TESTING  ONLY
+        // setPower(targetAltitude);
+    }
+
+
+    public float GetTarget()
+    {
+        return targetAltitude;
+    }
+
+    public float GetAltitude()
+    {
+        return transform.position.y;
+    }
+
+    public void setPower(float power)
+    {
+        power = power / 10;
+      
+        currentPower = power;
+    }
+
+}
